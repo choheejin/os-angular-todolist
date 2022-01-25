5a93cf38f228fc3a8810a7e80639d07427249779
wnstjd4778 <54669380+wnstjd4778@users.noreply.github.com>
2022-01-25 18:44:18 +0900

    commit
    commit

--- backend/routes/auth/controller.js
+++ backend/routes/auth/controller.js
@@ -1,189 +1,189 @@
-const User = require('../../schemas/user');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const { createResponse } = require('../../utils/response');
-const asyncHandler = require('express-async-handler');
-const { DUPLICATED_EMAIL, DUPLICATED_NICKNAME, DUPLICATED_TEL, EMAIL_NOT_FOUND,
-    INVAILED_EMAIL_OR_PASSWORD, INVALID_PASSWORD, PASSWORD_NOT_FOUND, FORBIDDEN, NOT_FOUND } = require('../../errors/index');
+// const User = require('../../schemas/user');
+// const bcrypt = require('bcrypt');
+// const jwt = require('jsonwebtoken');
+// const { createResponse } = require('../../utils/response');
+// const asyncHandler = require('express-async-handler');
+// const { DUPLICATED_EMAIL, DUPLICATED_NICKNAME, DUPLICATED_TEL, EMAIL_NOT_FOUND,
+//     INVAILED_EMAIL_OR_PASSWORD, INVALID_PASSWORD, PASSWORD_NOT_FOUND, FORBIDDEN, NOT_FOUND } = require('../../errors/index');
 
 
-// 회원가입하기
-const signUp = asyncHandler(async (req, res) => {
+// // 회원가입하기
+// const signUp = asyncHandler(async (req, res) => {
 
-    const { email, tel, password, nickName } = req.body;
+//     const { email, tel, password, nickName } = req.body;
 
-    const exUser = User.find({ 'email': email });
-    if (exUser) {
-        return next(DUPLICATED_EMAIL);
-    }
+//     const exUser = User.find({ 'email': email });
+//     if (exUser) {
+//         return next(DUPLICATED_EMAIL);
+//     }
 
-    exUser = User.find({ 'nickName': nickName });
-    if (exUser) {
-        return next(DUPLICATED_NICKNAME);
-    }
+//     exUser = User.find({ 'nickName': nickName });
+//     if (exUser) {
+//         return next(DUPLICATED_NICKNAME);
+//     }
 
-    exUser = User.find({ 'tel': tel });
-    if (exUser) {
-        return next(DUPLICATED_TEL);
-    }
+//     exUser = User.find({ 'tel': tel });
+//     if (exUser) {
+//         return next(DUPLICATED_TEL);
+//     }
 
-    const hash = await bcrypt.hash(password, 12);
-    await User.create({
-        email,
-        nickName,
-        password: hash,
-        tel
-    });
+//     const hash = await bcrypt.hash(password, 12);
+//     await User.create({
+//         email,
+//         nickName,
+//         password: hash,
+//         tel
+//     });
 
-    res.json(createResponse(res, ''));
-});
-
-
-// 로그인 하기
-const signIn = asyncHandler(async (req, res, next) => {
-    const { email, password } = req.body;
-
-    const user = await User.find({ 'email': email });
-    if (user.length) {
-        await bcrypt.compare(password, user[0].password, (err, result) => {
-            console.log(result);
-            if (result) {
-                const token = jwt.sign({
-                    user_email: user[0].email
-                }, process.env.JWT_SECRET, {
-                    expiresIn: '1h'
-                });
-                res.json(createResponse(res, { 'token': token }));
-            } else {
-                next(INVAILED_EMAIL_OR_PASSWORD);
-            }
-        });
-    } else {
-        next(INVAILED_EMAIL_OR_PASSWORD);
-    }
-});
+//     res.json(createResponse(res, ''));
+// });
 
 
-// 중복 아이디 체크하기
-const checkDuplicatedEmail = asyncHandler(async (req, res, next) => {
-    const { email } = req.body;
+// // 로그인 하기
+// const signIn = asyncHandler(async (req, res, next) => {
+//     const { email, password } = req.body;
 
-    const exUser = await User.find({ 'email': email });
-    if (exUser.length) {
-        next(DUPLICATED_EMAIL);
-    } else {
-        res.json(createResponse(res, ''));
-    }
-});
-
-
-// 중복 닉네임 체크하기
-const checkDuplicatedNickName = asyncHandler(async (req, res, next) => {
-    const { nickName } = req.body;
-
-    const exUser = await User.find({ 'nickName': nickName });
-    if (exUser.length) {
-        next(DUPLICATED_NICKNAME);
-    } else {
-        res.json(createResponse(res, ''));
-    }
-});
-
-// 중복 전화번호 체크하기
-const checkDuplicatedTel = asyncHandler(async (req, res, next) => {
-    const { tel } = req.body;
-
-    const exUser = await User.find({ 'tel': tel });
-    if (exUser.length) {
-        next(DUPLICATED_TEL);
-    } else {
-        res.json(createResponse(res, ''));
-    }
-});
+//     const user = await User.find({ 'email': email });
+//     if (user.length) {
+//         await bcrypt.compare(password, user[0].password, (err, result) => {
+//             console.log(result);
+//             if (result) {
+//                 const token = jwt.sign({
+//                     user_email: user[0].email
+//                 }, process.env.JWT_SECRET, {
+//                     expiresIn: '1h'
+//                 });
+//                 res.json(createResponse(res, { 'token': token }));
+//             } else {
+//                 next(INVAILED_EMAIL_OR_PASSWORD);
+//             }
+//         });
+//     } else {
+//         next(INVAILED_EMAIL_OR_PASSWORD);
+//     }
+// });
 
 
-// 전화번호와 이메일로 비밀번호 찾기(비밀번호가 복호화되서 나옴!!)
-const findPasswordByTelAndEmail = asyncHandler(async (req, res, next) => {
-    const { email, tel } = req.body;
+// // 중복 아이디 체크하기
+// const checkDuplicatedEmail = asyncHandler(async (req, res, next) => {
+//     const { email } = req.body;
+
+//     const exUser = await User.find({ 'email': email });
+//     if (exUser.length) {
+//         next(DUPLICATED_EMAIL);
+//     } else {
+//         res.json(createResponse(res, ''));
+//     }
+// });
+
+
+// // 중복 닉네임 체크하기
+// const checkDuplicatedNickName = asyncHandler(async (req, res, next) => {
+//     const { nickName } = req.body;
+
+//     const exUser = await User.find({ 'nickName': nickName });
+//     if (exUser.length) {
+//         next(DUPLICATED_NICKNAME);
+//     } else {
+//         res.json(createResponse(res, ''));
+//     }
+// });
+
+// // 중복 전화번호 체크하기
+// const checkDuplicatedTel = asyncHandler(async (req, res, next) => {
+//     const { tel } = req.body;
+
+//     const exUser = await User.find({ 'tel': tel });
+//     if (exUser.length) {
+//         next(DUPLICATED_TEL);
+//     } else {
+//         res.json(createResponse(res, ''));
+//     }
+// });
+
+
+// // 전화번호와 이메일로 비밀번호 찾기(비밀번호가 복호화되서 나옴!!)
+// const findPasswordByTelAndEmail = asyncHandler(async (req, res, next) => {
+//     const { email, tel } = req.body;
     
-    const exUser = await User.findOne({ 'tel': tel, 'email': email });
-    if (exUser) {
-        res.json(createResponse(res, { 'password': exUser.password }));
-    } else {
-        next(PASSWORD_NOT_FOUND);
-    }
-});
+//     const exUser = await User.findOne({ 'tel': tel, 'email': email });
+//     if (exUser) {
+//         res.json(createResponse(res, { 'password': exUser.password }));
+//     } else {
+//         next(PASSWORD_NOT_FOUND);
+//     }
+// });
 
 
-// 전화번호로 이메일 찾기
-const findEmailByTel = asyncHandler(async (req, res, next) => {
-    const { tel } = req.body;
+// // 전화번호로 이메일 찾기
+// const findEmailByTel = asyncHandler(async (req, res, next) => {
+//     const { tel } = req.body;
 
-    const exUser = await User.findOne({ 'tel': tel });
-    if (exUser) {
-        res.json(createResponse(res, { 'email': exUser.email }));
-    } else {
-        next(EMAIL_NOT_FOUND);
-    }
-});
-
-
-// 비밀번호가 맞는지 체크하고 변경하기
-const updatePassword = asyncHandler(async (req, res, next) => {
-    const { oldPassword, newPassword } = req.body;
-
-    const exUser = await User.findOne({ 'email': res.locals.email });
-    await bcrypt.compare(oldPassword, exUser.password, async (err, result) => {
-        if (result) {
-            const hash = await bcrypt.hash(newPassword, 12);
-            await User.updateOne({ '_id': exUser._id }, { 'password': hash });
-            res.json(createResponse(res, ''));
-        }
-        else {
-            next(INVALID_PASSWORD);
-        }
-    });
-});
+//     const exUser = await User.findOne({ 'tel': tel });
+//     if (exUser) {
+//         res.json(createResponse(res, { 'email': exUser.email }));
+//     } else {
+//         next(EMAIL_NOT_FOUND);
+//     }
+// });
 
 
-// 내정보 확인하기
-const getMyProfile = asyncHandler(async (req, res, next) => {
+// // 비밀번호가 맞는지 체크하고 변경하기
+// const updatePassword = asyncHandler(async (req, res, next) => {
+//     const { oldPassword, newPassword } = req.body;
 
-    const exUser = await User.findOne({ 'email': res.locals.email });
+//     const exUser = await User.findOne({ 'email': res.locals.email });
+//     await bcrypt.compare(oldPassword, exUser.password, async (err, result) => {
+//         if (result) {
+//             const hash = await bcrypt.hash(newPassword, 12);
+//             await User.updateOne({ '_id': exUser._id }, { 'password': hash });
+//             res.json(createResponse(res, ''));
+//         }
+//         else {
+//             next(INVALID_PASSWORD);
+//         }
+//     });
+// });
 
-    if(exUser) {
-        res.json(createResponse(res, exUser));
-    }
-    next(NOT_FOUND);
-});
+
+// // 내정보 확인하기
+// const getMyProfile = asyncHandler(async (req, res, next) => {
+
+//     const exUser = await User.findOne({ 'email': res.locals.email });
+
+//     if(exUser) {
+//         res.json(createResponse(res, exUser));
+//     }
+//     next(NOT_FOUND);
+// });
 
 
-// 닉네임 변경하기
-const updateNickName = asyncHandler(async (req, res, next) => {
-    const { nickName } = req.body;
+// // 닉네임 변경하기
+// const updateNickName = asyncHandler(async (req, res, next) => {
+//     const { nickName } = req.body;
     
-    const user = await User.findOne({ 'nickName': nickName });
-    if (user) {
+//     const user = await User.findOne({ 'nickName': nickName });
+//     if (user) {
 
-        return next(DUPLICATED_NICKNAME);
-    }
-    const exUser = await User.findOne({ 'email': res.locals.email });
-    if (exUser) {
-        await User.updateOne({ '_id': exUser._id }, { 'nickName': nickName });
-        res.json(createResponse(res, ''));
-    } else {
-        next(FORBIDDEN);
-    }
-});
+//         return next(DUPLICATED_NICKNAME);
+//     }
+//     const exUser = await User.findOne({ 'email': res.locals.email });
+//     if (exUser) {
+//         await User.updateOne({ '_id': exUser._id }, { 'nickName': nickName });
+//         res.json(createResponse(res, ''));
+//     } else {
+//         next(FORBIDDEN);
+//     }
+// });
 
 
-exports.signUp = signUp;
-exports.signIn = signIn;
-exports.checkDuplicatedEmail = checkDuplicatedEmail;
-exports.checkDuplicatedNickName = checkDuplicatedNickName;
-exports.checkDuplicatedTel = checkDuplicatedTel;
-exports.findPasswordByTelAndEmail = findPasswordByTelAndEmail;
-exports.findEmailByTel = findEmailByTel;
-exports.updatePassword = updatePassword;
-exports.getMyProfile = getMyProfile;
-exports.updateNickName = updateNickName;
+// exports.signUp = signUp;
+// exports.signIn = signIn;
+// exports.checkDuplicatedEmail = checkDuplicatedEmail;
+// exports.checkDuplicatedNickName = checkDuplicatedNickName;
+// exports.checkDuplicatedTel = checkDuplicatedTel;
+// exports.findPasswordByTelAndEmail = findPasswordByTelAndEmail;
+// exports.findEmailByTel = findEmailByTel;
+// exports.updatePassword = updatePassword;
+// exports.getMyProfile = getMyProfile;
+// exports.updateNickName = updateNickName;
--- backend/routes/todo/index.js
+++ backend/routes/todo/index.js
@@ -4,15 +4,14 @@ const controller = require('./controller');
 const {verifyToken} = require('../../middlewares/authorization');
 
 
-router.post('/', verifyToken, controller.createTodo);
-router.delete('/:id', verifyToken, controller.deleteTodo);
-router.put('/:id', verifyToken, controller.updateTodo);
-router.get('/today', verifyToken, controller.getTodayTodo);
-router.get('/important/:id', verifyToken, controller.updateImportantById);
-router.get('/completed/:id', verifyToken, controller.updateCompletedById);
-router.get('/all', verifyToken, controller.getTodoByDay);
-router.get('/important', verifyToken, controller.getImportantTodo);
-router.get('/month', verifyToken, controller.getTodoCountByMonth);
+router.post('/', controller.createTodo);
+router.delete('/:id', controller.deleteTodo);
+router.put('/:id', controller.updateTodo);
+router.get('/today', controller.getAllTodayTodo);
+router.get('/completed/:id', controller.updateCompletedById);
+router.get('/day', controller.getTodoByDay);
+router.get('/today/complited', controller.getComplitedTodayTodo);
+router.get('/today/notComplited', controller.getNotComplitedTodayTodo);
 
 
 module.exports = router;
