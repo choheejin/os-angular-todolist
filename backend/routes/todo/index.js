d9829ace7055334e7da61389c53ddcfea610c9db
wnstjd4778 <54669380+wnstjd4778@users.noreply.github.com>
2022-01-30 11:17:39 +0900

    Refactor: 함수

--- backend/routes/todo/index.js
+++ backend/routes/todo/index.js
@@ -7,12 +7,6 @@ const {verifyToken} = require('../../middlewares/authorization');
 router.post('/', controller.createTodo);
 router.delete('/:id', controller.deleteTodo);
 router.put('/:id', controller.updateTodo);
-router.get('/today', controller.getAllTodayTodo);
-router.get('/completed/:id', controller.updateCompletedById);
-// router.get('/day', controller.getTodoByDay);
-// router.get('/today/completed', controller.getCompletedTodayTodo);
-// router.get('/today/notCompleted', controller.getNotCompletedTodayTodo);
-// router.get('/all' , controller.getAllTodo);
 router.get('/', controller.getTodo);
 
 module.exports = router;
